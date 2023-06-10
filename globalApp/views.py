b7ac0dee947524f926c839424e2dcb918f62f27d
Cristhian Agamez <93165832+CristhianAC@users.noreply.github.com>
2023-06-09 23:45:49 -0500

    Cambio de forms

--- globalApp/views.py
+++ globalApp/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from BackEnd.Procesos import Procesos
-from .forms import ButtonSelector
+from .forms import ButtonSelector, Searching
 procesos = Procesos()
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 def proyect(request):
-    if request.method == 'GET':
-        context = procesos.context
+    if request.method == "GET":
+        context = procesos.obternerHTML()
         context["formDs"] = ButtonSelector
+        context["search"] = Searching
         return render(request, "proyect.html", context)
     else:
-        pass
+        
+        context = procesos.obternerHTML(request.POST['dataset'])
+        context["formDs"] = ButtonSelector
+        context["search"] = Searching
+        return render(request, "proyect.html", context)
