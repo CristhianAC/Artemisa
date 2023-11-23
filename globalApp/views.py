847af8d05aad3bd85711312f201dbc4ea2cbe7ac
Cristhian Agamez <93165832+CristhianAC@users.noreply.github.com>
2023-11-22 23:55:04 -0500

    A

--- globalApp/views.py
+++ globalApp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import modelEjecute
-
+from Modelo import Modelo
 
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 def proyect(request):
-    
+    modelo = Modelo()
     if request.method == 'POST':
         form = modelEjecute(request.POST)
         if form.is_valid():
